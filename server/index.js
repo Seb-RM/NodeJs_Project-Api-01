689bbd607f41ef37e94ee5e90cf4448d44b0c7c3
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-08-06 22:20:02 -0300

    configurar router principal

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const productsRouter = require('../routes/products');
+const usersRouter = require('../routes/users');
+const express = require("express");
+
+function apiRouter (app) {
+    const router = express.Router();
+    app.use('/api/v1', router);
+
+    router.use('/products', productsRouter);
+    router.use('/users', usersRouter);
+}
+
+module.exports = apiRouter;

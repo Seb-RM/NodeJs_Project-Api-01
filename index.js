d0d5bb78c7bbac5585717b984ca09c8957c835f7
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-08-06 22:19:28 -0300

    configurar servidor

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Funcionando")
+const express = require("express");
+const apiRouter = require("./server");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/',(req, res)=> {
+    res.send('Funcionando en ruta raíz.')
+});
+
+apiRouter(app);
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
+});

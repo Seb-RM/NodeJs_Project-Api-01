537339bf247a9df465ab803a1589a1c37ec0d45c
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-08-11 21:49:38 -0300

    configurar keroku para funcionar localmente

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const apiRouter = require("./server");
 const cors = require("cors");
 const { errorLogs, handlerError } = require("./middleware/error.handler");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());

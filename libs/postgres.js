f44d8761aa89a54cfc722f6d9c1e8abc97023855
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-08-13 21:48:44 -0300

    cambiar configuracion para funcionar con pool

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,15 +1,25 @@
-const { Client } = require("pg");
+const { Pool } = require("pg");
 
-async function getConnection() {
-    const client = new Client({
-        host: "localhost",
-        port: "5432",
-        user: "admin",
-        password: "admin1234",
-        database: "my_api",
-    });
-    await client.connect();
-    return client;
-}
+// async function getConnection() {
+//     const client = new pg.Client({
+//         host: "localhost",
+//         port: "5432",
+//         user: "admin",
+//         password: "admin1234",
+//         database: "my_api",
+//     });
+//     await client.connect();
+//     return client;
+// }
 
-module.exports = getConnection;
+// module.exports = getConnection;
+
+const pool = new Pool({
+    host: "localhost",
+    port: 5432,
+    user:"admin",
+    password: "admin1234",
+    database: "my_api"
+});
+
+module.exports = pool;
--- services/servicesUsers.js
+++ services/servicesUsers.js
@@ -1,4 +1,5 @@
-const getConnection = require("../libs/postgres");
+// const getConnection = require("../libs/postgres");
+const pool = require("../libs/postgres");
 
 // const getAllUsers = async (req) => {
 //     try {
@@ -17,7 +18,7 @@ const getConnection = require("../libs/postgres");
 // };
 
 const getAllUsers = async (req, res) => {
-    const client = await getConnection();
+    const client = await pool();
     const response = await client.query('SELECT * FROM tasks');
     return response.rows;
 }

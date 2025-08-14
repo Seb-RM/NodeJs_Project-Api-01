6e35eb60d2a2f50f332b199eb9e00880ea403ffa
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-08-13 21:38:13 -0300

    cambiar configuracion servicios y rutas para interactuar con base de datos

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const getAllUsers = require("../services/servicesUsers");
+const serviceUsers = require("../services/servicesUsers");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+// router.get("/", async (req, res) => {
+//     try {
+//         const users = await getAllUsers(req);
+//         res.json(users);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+router.get('/', async (req, res, next)=> {
     try {
-        const users = await getAllUsers(req);
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+        const getUsers = await serviceUsers.getAllUsers(req,res);
+        return res.send({getUsers})
+    } catch(error){
+        next(error)
     }
 });
 
--- services/servicesUsers.js
+++ services/servicesUsers.js
@@ -1,17 +1,25 @@
-const getAllUsers = async (req) => {
-    try {
-        const { limit, offset } = req.query;
-        if (limit && offset) {
-            return {
-                limit,
-                offset,
-            };
-        } else {
-            return { message: "No hay ningún parámetro." };
-        }
-    } catch (error) {
-        throw new Error("Error al obtener usuarios");
-    }
-};
+const getConnection = require("../libs/postgres");
 
-module.exports = getAllUsers;
+// const getAllUsers = async (req) => {
+//     try {
+//         const { limit, offset } = req.query;
+//         if (limit && offset) {
+//             return {
+//                 limit,
+//                 offset,
+//             };
+//         } else {
+//             return { message: "No hay ningún parámetro." };
+//         }
+//     } catch (error) {
+//         throw new Error("Error al obtener usuarios");
+//     }
+// };
+
+const getAllUsers = async (req, res) => {
+    const client = await getConnection();
+    const response = await client.query('SELECT * FROM tasks');
+    return response.rows;
+}
+
+module.exports = {getAllUsers};

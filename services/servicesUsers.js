77a80de3400c61e0b2fe2dfd806d8687449171d5
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-08-15 22:38:56 -0300

    crear servicios para users

--- services/servicesUsers.js
+++ services/servicesUsers.js
@@ -35,4 +35,54 @@ const getAllUsers = async (req, res) => {
     return response
 }
 
-module.exports = {getAllUsers};
+const createUser = async (body) => {
+    try {
+        const newUser = await models.User.create(body);
+        return {
+            user: newUser,
+            message: "User created."
+        }
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+const updateUser = async ({id, body}) => {
+    try {
+        const user = await models.User.findByPk(id);
+        if(!user) {
+            return {
+                error: "User not found."
+            }
+        }
+        const response = await user.update(body);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const deleteUser = async (id) => {
+    try {
+        const user = await models.User.findOne(id);
+        if(!user) {
+            return {
+                error: "User not found."
+            }
+        }
+        await user.destroy();
+        return {
+            message: "User deleted.",
+            id
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = {
+    getAllUsers,
+    createUser,
+    updateUser,
+    deleteUser
+};

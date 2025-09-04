c3e765841d7ba57ffe610bac4ae8c50664d71c89
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-09-03 23:21:32 -0300

    agregar modelos y esquemas para categorías y productos en la configuración de modelos

--- db/models/index.js
+++ db/models/index.js
@@ -1,13 +1,19 @@
 const { User, UserSchema } = require("./user.model");
 const { Client, ClientSchema } = require("./client.model");
+const { Category, CategorySchema } = require("./category.model");
+const { Product, ProductSchema } = require("./product.model");
 
 function setupModels(sequelize) {
     User.init(UserSchema, User.config(sequelize));
     Client.init(ClientSchema, Client.config(sequelize));
+    Category.init(CategorySchema, Category.config(sequelize));
+    Product.init(ProductSchema, Product.config(sequelize));
 
 
     User.associate(sequelize.models);
     Client.associate(sequelize.models);
+    Category.associate(sequelize.models);
+    Product.associate(sequelize.models);
 }
 
 
